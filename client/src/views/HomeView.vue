e4e65773465f44f03a2b8cd8bc396822eff1c1dd
imrany <imranmat254@gmail.com>
2023-10-07 12:55:06 +0300

    error

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -5,7 +5,11 @@
 <template>
     <LayoutGrid>
         <template #grid-2>
-           
+           <form class="fixed bottom-0 bg-gray-800 p-6 w-full h-[80px]">
+           <div class="flex">
+
+           </div>
+                      </form>
         </template>
     </LayoutGrid>
 </template>
