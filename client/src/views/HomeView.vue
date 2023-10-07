79aa2b45ea1e4373fd2ec5dc87e3c207f018525f
imrany <imranmat254@gmail.com>
2023-10-07 12:55:16 +0300

    Revert "error"
    This reverts commit e4e65773465f44f03a2b8cd8bc396822eff1c1dd.

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -5,11 +5,7 @@
 <template>
     <LayoutGrid>
         <template #grid-2>
-           <form class="fixed bottom-0 bg-gray-800 p-6 w-full h-[80px]">
-           <div class="flex">
-
-           </div>
-                      </form>
+           
         </template>
     </LayoutGrid>
 </template>
